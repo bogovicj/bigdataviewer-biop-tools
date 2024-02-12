04ddc738f48d6d9dd820f289c79d741ec7d03d52
John Bogovic <bogovicj@janelia.hhmi.org>
2024-02-12 16:50:11 -0500

    update AlphaSourceWarped for bigwarp 9 WarpedSource
    * only testing so far was with AlphaDemoFull test

--- src/main/java/bdv/util/source/alpha/AlphaSourceWarped.java
+++ src/main/java/bdv/util/source/alpha/AlphaSourceWarped.java
@@ -7,6 +7,7 @@ import net.imglib2.*;
 import net.imglib2.position.FunctionRandomAccessible;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.realtransform.RealTransformRealRandomAccessible;
+import net.imglib2.realtransform.RealTransformSequence;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
@@ -42,28 +43,35 @@ public class AlphaSourceWarped extends AlphaSource {
         return origin_alpha;
     }
 
-    @Override
-    public RandomAccessibleInterval<FloatType> getSource(int t, int level) {
-        final float finalAlpha = alpha;
-
-        final RandomAccessible< FloatType > randomAccessible =
-                new FunctionRandomAccessible<>( 3, () -> (loc, out) -> out.setReal( finalAlpha ), FloatType::new );
-        // Giving singular matrix issue!!
-        return Views.interval(randomAccessible, new FinalInterval(new long[]{0,0,0}, new long[]{1,1,1})); // CAUSES ERROR : SINGULAR MATRIX origin.getSource(t, level));
-    }
+	@Override
+	public RandomAccessibleInterval<FloatType> getSource(int t, int level) {
+		final RealRandomAccessible<FloatType> rra = getInterpolatedSource(t, level, Interpolation.NEARESTNEIGHBOR);
+		return Views.interval(Views.raster(rra), origin_warped.getSource(t, level));
+	}
 
     @Override
     public RealRandomAccessible<FloatType> getInterpolatedSource(int t, int level, Interpolation method) {
-        RealRandomAccessible<FloatType> sourceRealAccessible =
-                getAlpha().getInterpolatedSource(t, level, method);
-        if (origin_warped.isTransformed()) {
-            AffineTransform3D transform = new AffineTransform3D();
-            getAlpha().getSourceTransform(t, level, transform);
-            RealRandomAccessible<FloatType> srcRaTransformed = RealViews.affineReal(getAlpha().getInterpolatedSource(t, level, method), transform);
-            return (RealRandomAccessible)(origin_warped.getTransform() == null ? srcRaTransformed : new RealTransformRealRandomAccessible(srcRaTransformed, origin_warped.getTransform()));
-        } else {
-            return sourceRealAccessible;
-        }
+		RealRandomAccessible<FloatType> sourceRealAccessible = getAlpha().getInterpolatedSource(t, level, method);
+		if (origin_warped.isTransformed()) {
+
+			final AffineTransform3D transform = new AffineTransform3D();
+			getAlpha().getSourceTransform(t, level, transform);
+			final RealRandomAccessible<FloatType> srcRa = getAlpha().getInterpolatedSource(t, level, method);
+
+			if (origin_warped.getTransform() == null)
+				return srcRa;
+			else {
+				final RealTransformSequence seq = new RealTransformSequence();
+				// build the inverse transform
+				seq.add(transform);
+				seq.add(origin_warped.getTransform().copy());
+				seq.add(transform.inverse());
+				return new RealTransformRealRandomAccessible(srcRa, seq);
+			}
+
+		} else {
+			return sourceRealAccessible;
+		}
     }
 
     @Override
